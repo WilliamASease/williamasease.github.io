666f5e590d5fc4b83875a679146b87bd1f5e25d3
WilliamASease <54331964+WilliamASease@users.noreply.github.com>
2021-01-01 19:57:40 -0500

    tabs

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 console.log("I feel good. Everything is fine.");
+
+var adj = ["German Engineering","Perfectly Fine","Right Honorable","Best in Show"];
+document.getElementById("adjective").innerHTML = adj[Math.floor(Math.random() * adj.length)] + "!";
+
+var pas = ["Graphic Design","Pottery","Cashiering","Landscaping","Literary Criticism","Kayaking","Journalism"];
+document.getElementById("formerPassion").innerHTML = pas[Math.floor(Math.random() * pas.length)];
