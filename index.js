8e483495021f8517223bbcc81d5d07c71995e224
WilliamASease <54331964+WilliamASease@users.noreply.github.com>
2021-02-20 18:12:20 -0500

    remove unfunny joke

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 console.log("I feel good. Everything is fine.");
 
-var adj = ["German Engineering","Perfectly Fine","Right Honorable","Best in Show","Whole Grain","Post Rock","Easy Rider"];
-document.getElementById("adjective").innerHTML = adj[Math.floor(Math.random() * adj.length)] + "!";
+//var adj = ["German Engineering","Perfectly Fine","Right Honorable","Best in Show","Whole Grain","Post Rock","Easy Rider"];
+//document.getElementById("adjective").innerHTML = adj[Math.floor(Math.random() * adj.length)] + "!";
 
 var pas = ["Graphic Design","Pottery","Cashiering","Landscaping","Literary Criticism","Kayaking","Journalism","Koala Raising","Hammocking"];
 document.getElementById("formerPassion").innerHTML = pas[Math.floor(Math.random() * pas.length)];
