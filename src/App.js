a5ca4036cc5c1e674c40293d95fb4b133ed59f5d
WilliamASease <seasewa@appstate.edu>
2022-04-07 00:53:45 -0500

    Revert "Hide React from github.io"
    This reverts commit 0d632515e6c804210b8c5e3144c44df754d902c4.

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Site from "./Site/Site.tsx";
+
+function App() {
+  return (
+    <Site />
+  );
+}
+
+export default App;
---
+++ src/Site/Site.tsx
@@ -0,0 +1,5 @@
+import React, { ReactElement } from "react";
+
+export default function Site(): ReactElement {
+    return (<div>HELLO?</div>);
+}
