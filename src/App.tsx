3ac07f78005c9868e8c76df21daa265332714823
WilliamASease <seasewa@appstate.edu>
2023-12-20 02:42:21 -0500

    work

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { FlexBox, WindowShade } from "./sdk/CommonComponents";
+import { Anchor, FlexBox, WindowShade } from "./sdk/CommonComponents";
 import { isMobile } from "react-device-detect";
 
 function App() {
@@ -12,11 +12,31 @@ function App() {
     >
       <FlexBox
         orientation="column"
-        style={{ width: isMobile ? "100%" : 1000, backgroundColor: "white" }}
+        style={{
+          width: isMobile ? "100%" : 1000,
+          backgroundColor: "white",
+          paddingLeft: 5,
+          paddingRight: 5,
+        }}
         fullHeight
       >
-        <WindowShade title="Hello">
-          <div>Goodbye</div>
+        <div>William A. Sease's Personal Site</div>
+        <WindowShade title="The Important Links">
+          <Anchor link="https://www.linkedin.com/in/william-sease-907bb219a?trk=people-guest_people_search-card">
+            Linkedin
+          </Anchor>
+          <Anchor link="mailto:WilliamASease@protonmail.com">Email</Anchor>
+          <Anchor link="https://github.com/WilliamASease">Github</Anchor>
+          <Anchor link="https://williamasease.github.io/seaseResume.pdf">
+            Resume (PDF)
+          </Anchor>
+        </WindowShade>
+        <WindowShade title="File Photo">
+          <img
+            src="https://williamasease.github.io/build/images/was.png"
+            alt="William A. Sease"
+            style={{ borderRadius: 10 }}
+          />
         </WindowShade>
       </FlexBox>
     </FlexBox>
--- src/sdk/CommonComponents.tsx
+++ src/sdk/CommonComponents.tsx
@@ -22,28 +22,46 @@ export const FlexBox = (props: FlexBoxProps) => (
   </div>
 );
 
-type WindowShadeProps = {
-  children: ReactNode;
-  title: string;
-};
-
-export const WindowShade = (props: WindowShadeProps) => {
+export const WindowShade = (props: { children: ReactNode; title: string }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <FlexBox orientation="column">
+    <FlexBox orientation="column" style={{ marginTop: 5 }}>
       <FlexBox orientation="row">
-        <span
-          style={{ backgroundColor: "green" }}
-          onClick={() => setIsOpen(!isOpen)}
-        >
-          {isOpen ? "V" : ">"}
-        </span>
-        <div>
-          <span style={{ backgroundColor: "red" }}>{props.title}</span>
+        <div style={{}} onClick={() => setIsOpen(!isOpen)}>
+          <div
+            style={{
+              border: "solid black 1px",
+              width: 20,
+              height: 20,
+            }}
+          >
+            <img
+              style={{ width: 20, height: 20 }}
+              src={`https://williamasease.github.io/build/images/${
+                isOpen ? `minus` : `plus`
+              }.png`}
+            />
+          </div>
+        </div>
+        <div style={{ marginLeft: 5 }}>
+          <div style={{ fontWeight: "bold", marginBottom: 5 }}>
+            {props.title}
+          </div>
           {isOpen && props.children}
         </div>
       </FlexBox>
     </FlexBox>
+  );
+};
+
+export const Anchor = (props: { children: ReactNode; link: string }) => {
+  const { children, link } = props;
+  return (
+    <div>
+      <a href={link} target="_blank">
+        <div>{children}</div>
+      </a>
+    </div>
   );
 };
