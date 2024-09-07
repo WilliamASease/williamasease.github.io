fec8e28f9e61a3e4f285ec5fd170c14fafbf9598
WilliamASease <WilliamASease@gmail.com>
2024-09-06 23:36:46 -0400

    commit

--- src/components/ComponentSwitcher.tsx
+++ src/components/ComponentSwitcher.tsx
@@ -1,5 +1,6 @@
 import { ReactElement, ReactNode, useState } from "react";
 import { FlexBox, SiteText } from "../sdk/CommonComponents";
+import { isMobile } from "react-device-detect";
 
 type ComponentPair = {
   key: string;
@@ -18,21 +19,27 @@ export const ComponentSwitcher = (props: IProps) => {
   return (
     <div>
       <div style={{ margin: 10, padding: 10, border: "solid black 3px" }}>
-        {pairs.map((p, i) => (
-          <span
-            style={{
-              marginRight: 5,
-              padding: 2,
-              cursor: "pointer",
-              color: active === i ? "black" : "green",
-              backgroundColor: active === i ? 'white' : 'black',
-              border:'solid green 1px'
-            }}
-            onClick={() => setActive(i)}
+        {pairs.map((p, i) => {
+          const props = {style:{
+            marginRight: 5,
+            padding: 2,
+            cursor: "pointer",
+            color: active === i ? "black" : "green",
+            backgroundColor: active === i ? 'white' : 'black',
+            border:'solid green 1px'
+          },
+          onClick:() => setActive(i)}
+          return (
+          isMobile ? <div
+          {...props}
+        >
+          {p.key}
+        </div> : <span
+            {...props}
           >
             {p.key}
           </span>
-        ))}
+        )})}
       </div>
       {active !== -1 && (
         <FlexBox
